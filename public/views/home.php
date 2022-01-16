632538e356da7faccf419b81724b2c7ca7c16ab2
Nadji <nadjitan@yahoo.com.ph>
2022-01-16 17:31:56 +0800

    Test session

--- public/views/home.php
+++ public/views/home.php
@@ -1,3 +1,3 @@
 <div class="home">
-  
+  <?php echo $_SESSION['test'] ?>
 </div>
--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -4,13 +4,16 @@ abstract class HomeController extends Controller {
     
     public static function index() {
         $bachelorsDegree = new BachelorsDegree();
+        $dataToShow = '';
 
         foreach ($bachelorsDegree->getBachelorsDegrees() as $bd) {
             $obj = $bd['id'];
             
-            echo <<< HTML
+            $dataToShow = $dataToShow. <<< HTML
                 <div>$obj</div>
             HTML;
         }
+
+        $_SESSION['test'] = $dataToShow;
     }
 }
